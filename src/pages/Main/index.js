dacc939d8c8dd432f0d01a990aedad5a823df080
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2020-02-13 21:19:45 -0300

    Animacao Finalizada

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import { Animated } from 'react-native';
 import {
   Container,
@@ -20,6 +20,8 @@ import Tabs from '~/components/Tabs';
 import Menu from '~/components/Menu';
 
 export default function Main() {
+  let offset = 0;
+
   const translateY = new Animated.Value(0);
   const animatedEvent = Animated.event(
     [
@@ -33,7 +35,29 @@ export default function Main() {
       useNativeDriver: true,
     },
   );
-  const onHandlerStateChanged = event => {};
+  const onHandlerStateChanged = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+      offset += translationY;
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        opened = false;
+        translateY.setOffset(0);
+        translateY.setValue(offset);
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  };
 
   return (
     <Container>
